f747f6867e084cebf293734ae18079a8dec62b73
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2018-12-19 17:56:17 -0500

    created EmailSummary component

--- src/components/emails/EmailList.js
+++ src/components/emails/EmailList.js
@@ -1,32 +1,14 @@
 import React from 'react'
+import EmailSummary from './EmailSummary'
 
 const EmailList = () => {
   return (
     <div className="project-list section">
 
-      <div className="card z-depth-0 projet-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by John Smith</p>
-          <p className="grey-text">19th of December, 5pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 projet-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by John Smith</p>
-          <p className="grey-text">19th of December, 5pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 projet-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by John Smith</p>
-          <p className="grey-text">19th of December, 5pm</p>
-        </div>
-      </div>
+      <EmailSummary />
+      <EmailSummary />
+      <EmailSummary />
+      <EmailSummary />
 
     </div>
   )
---
+++ src/components/emails/EmailSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const EmailSummary = () => {
+  return (
+    <div className="card z-depth-0 projet-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by John Smith</p>
+        <p className="grey-text">19th of December, 5pm</p>
+      </div>
+    </div>
+  )
+}
+
+export default EmailSummary
