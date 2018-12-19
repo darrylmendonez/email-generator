de59ed4c7e280a79ff1d17bfa7fd6dfa913fa417
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2018-12-19 18:29:30 -0500

    created LogIn and Register components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import EmailDetails from './components/emails/EmailDetails'
+import LogIn from './components/auth/LogIn'
+import Register from './components/auth/Register'
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route path='/email/:id' component={EmailDetails} />
+            <Route path='/login' component={LogIn} />
+            <Route path='/register' component={Register} />
           </Switch>
         </div>
       </BrowserRouter>
