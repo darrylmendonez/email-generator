eb2b67fd16b9459c2b82d04ca8f0f1670e869a31
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-01-04 15:01:58 -0500

    added in error messages for when user doesnt register properly

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -20,7 +20,7 @@ class Register extends Component {
     this.props.register(this.state)
   }
   render() {
-    const { auth } = this.props;
+    const { auth, authError } = this.props;
     if (auth.uid) return <Redirect to='/' />
 
     return (
@@ -46,6 +46,9 @@ class Register extends Component {
             </div>
             <div className="input-field">
               <button className="btn pink lighten-1 z-depth-0">Register</button>
+              <div className="red-text center">
+                { authError ? <p>{ authError }</p> : null }
+              </div>
             </div>
           </form>
         </div>
@@ -56,7 +59,8 @@ class Register extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   }
 }
 
